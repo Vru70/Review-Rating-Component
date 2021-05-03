e20604b98ebde98311e0a9a9896679cd651346af
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-05-04 00:58:35 +0530

    rarting star logic

--- force-app/main/default/lwc/callFiveStar/callFiveStar.js
+++ force-app/main/default/lwc/callFiveStar/callFiveStar.js
@@ -1,16 +1,17 @@
 /**
  * @author            : Vrushabh Uprikar
- * @last modified on  : 04-20-2021
+ * @last modified on  : 05-04-2021
  * @last modified by  : Vrushabh Uprikar
  * Modifications Log 
  * Ver   Date         Author             Modification
  * 1.0   04-20-2021   Vrushabh Uprikar   Initial Version
 **/
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
 export default class CallFiveStar extends LightningElement
 {
-    rating;
+    @track rating;
+    @track lessthanthree = false;
     connectedCallback()
     {
         this.rating = 0;
@@ -19,5 +20,13 @@ export default class CallFiveStar extends LightningElement
     handleRatingChanged(event)
     {
         console.log('Rating : ' + event.detail.rating);
+        var ratingval = event.detail.rating;
+        if (ratingval <= 3)
+        {
+            this.lessthanthree = true;
+        } else if (ratingval > 3)
+        {
+            this.lessthanthree = false;
+        }
     }
 }
